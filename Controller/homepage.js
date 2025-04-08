65f431a6f0fb7d795441b8cb524f111f744684f7
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-08 14:36:44 +0530

    added auth in showProfiles

--- Controller/homepage.js
+++ Controller/homepage.js
@@ -1,9 +1,14 @@
 const profile = require("../Model/userProfile");
-const userProfile = require("../Model/userProfile")
+const userProfile = require("../Model/userProfile");
+const { Op } = require('sequelize');
 
 const findProfiles = async (req, res) => {
     try {
-        let data = await userProfile.findAll();
+        let data = await userProfile.findAll({where:{
+            userId:{
+                [Op.ne]:req.user.id
+            }
+        }});
         res.status(200).json({ profile: data })
     } catch (error) {
         console.log(error)
--- Routes/homepage.js
+++ Routes/homepage.js
@@ -3,6 +3,6 @@ const authentication = require("../Middlewares/authentication.js")
 const router = express.Router();
 const profileController = require("../Controller/homepage.js")
 
-router.get("/showProfiles",profileController)
+router.get("/showProfiles",authentication,profileController)
 
 module.exports=router
