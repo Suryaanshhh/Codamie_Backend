2edd8f725f156d361d6fcd5201942c259427e385
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 16:56:52 +0530

    back to older versions

--- Routes/githuUserRoutes.js
+++ Routes/githuUserRoutes.js
@@ -3,7 +3,7 @@ const passport = require("../Controller/githubAuth");
 const User = require("../Model/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-router.options("*", (_, res) => res.sendStatus(200));
+
 router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
 
 
@@ -11,36 +11,34 @@ router.get(
     "/github/callback",
     passport.authenticate("github", { failureRedirect: "/" }),
     async (req, res) => {
-      try {
-        const { id, displayName } = req.user;
-        let user = await User.findOne({ where: { githubID: id } });
-  
-        let isNewUser = false;
-  
-        if (!user) {
-          user = await User.create({
-            Name: displayName,
-            githubID: id,
-          });
-          isNewUser = true;
+        try {
+            const { id, displayName } = req.user
+            console.log(id, displayName)
+            let user = await User.findOne({ where: { githubID: id } })
+
+            if (!user) {
+                let newUser = await User.create({
+                    Name: displayName,
+                    githubID: id
+                })
+                const token = await jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra")
+                
+                res.redirect(`http://localhost:5173/createProfile?token=${token}`);
+            }
+
+            else {
+                const token = await jwt.sign({ userID: user.dataValues.id }, "abra ka dabra")
+                res.redirect(`http://localhost:5173/home?token=${token}`);
+            }
         }
-  
-        const token = jwt.sign({ userID: user.dataValues.id }, "abra ka dabra");
-  
-        // ✅ Return a script that sets localStorage and closes the popup
-        return res.send(`
-          <script>
-            localStorage.setItem("githubToken", "${token}");
-            localStorage.setItem("isNewUser", "${isNewUser}");
-            window.close();
-          </script>
-        `);
-      } catch (err) {
-        console.error(err);
-        return res.status(500).send("Something went wrong");
-      }
+        catch (err) {
+            console.log(err)
+            res.status(500).json({ message: "somthing went wrong" })
+        }
+
     }
-  );
+);
+
 
 // router.get("/profile", (req, res) => {
 //     if (!req.isAuthenticated()) return res.redirect("/");
