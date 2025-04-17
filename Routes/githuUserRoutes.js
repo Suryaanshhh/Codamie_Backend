a508dbb4543d16fa2e2358e57b00af9f46de9ee1
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 17:28:35 +0530

    debugging #2

--- Routes/githuUserRoutes.js
+++ Routes/githuUserRoutes.js
@@ -9,55 +9,66 @@ router.get("/github", passport.authenticate("github", { scope: ["user:email"] })
 
 router.get(
     "/github/callback",
-    passport.authenticate("github", { failureRedirect: "/" }),
-    async (req, res) => {
-        try {
-            const { id, displayName } = req.user;
-            let user = await User.findOne({ where: { githubID: id } });
-            let token;
-
-            if (!user) {
-                let newUser = await User.create({
-                    Name: displayName,
-                    githubID: id
-                });
-                token = await jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra");
-                // Send HTML with auto-redirect instead of direct redirect
-                return res.send(`
-                    <html>
-                        <head>
-                            <script>
-                                window.location.href = 'http://localhost:5173/createProfile?token=${token}';
-                            </script>
-                        </head>
-                        <body>
-                            <p>Redirecting to application...</p>
-                        </body>
-                    </html>
-                `);
-            } else {
-                token = await jwt.sign({ userID: user.dataValues.id }, "abra ka dabra");
-                // Send HTML with auto-redirect instead of direct redirect
-                return res.send(`
-                    <html>
-                        <head>
-                            <script>
-                                window.location.href = 'http://localhost:5173/home?token=${token}';
-                            </script>
-                        </head>
-                        <body>
-                            <p>Redirecting to application...</p>
-                        </body>
-                    </html>
-                `);
+    (req, res, next) => {
+        // Handle the authentication without redirect
+        passport.authenticate("github", (err, user, info) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Authentication error");
             }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: "something went wrong" });
-        }
+            
+            if (!user) {
+                return res.redirect("/"); // Failure case
+            }
+            
+            req.login(user, async (err) => {
+                if (err) return next(err);
+                
+                try {
+                    const { id, displayName } = user;
+                    let dbUser = await User.findOne({ where: { githubID: id } });
+                    let token;
+                    let redirectPath;
+
+                    if (!dbUser) {
+                        let newUser = await User.create({
+                            Name: displayName,
+                            githubID: id
+                        });
+                        token = jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra");
+                        redirectPath = "createProfile";
+                    } else {
+                        token = jwt.sign({ userID: dbUser.dataValues.id }, "abra ka dabra");
+                        redirectPath = "home";
+                    }
+
+                    // Return an HTML page that will handle the redirect client-side
+                    res.send(`
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <title>Authentication Successful</title>
+                        </head>
+                        <body>
+                            <p>Authentication successful. Redirecting...</p>
+                            <script>
+                                // Store token in localStorage
+                                localStorage.setItem('auth_token', '${token}');
+                                
+                                // Redirect to the appropriate page
+                                window.location.href = 'http://localhost:5173/${redirectPath}';
+                            </script>
+                        </body>
+                        </html>
+                    `);
+                } catch (err) {
+                    console.error(err);
+                    res.status(500).send("Something went wrong");
+                }
+            });
+        })(req, res, next);
     }
 );
-
 // router.get("/profile", (req, res) => {
 //     if (!req.isAuthenticated()) return res.redirect("/");
 //     res.json({ user: req.user });
