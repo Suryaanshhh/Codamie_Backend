f3dee424a180241c7b59850e5e5823a71e791d4f
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 15:09:47 +0530

    changed to common js for railway

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
-import express from "express";
-import http from "http";
-import bodyParser from "body-parser";
-import cors from "cors"
-import { Server } from "socket.io";
-import db from "./Database/mySql.js"
-import User from "./Model/user.js";
-import profile from "./Model/userProfile.js";
-import matchesRequest from "./Model/matchesRequest.js";
-import userMatchesRoutes from "./Routes/userMatches.js"
-import userMatches from "./Model/matches.js"
-import userRoute from "./Routes/userRoutes.js"
-import session from "express-session";
-import passport from "./Controller/githubAuth.js"
-import githubUserRoutes from "./Routes/githuUserRoutes.js"
-import homePageRoutes from "./Routes/homepage.js"
-import matchesRequestroutes from "./Routes/matchesRequestRoutes.js"
-import messageRoutes from "./Routes/messageRoutes.js"
-import aiBotRoutes from "./Routes/botRoutes.js"
-import setupSocketServer from "./Middlewares/sockets.js"
-import cron from "node-cron"
-
-
-// const session = require("express-session");
-// const passport = require("./github-auth/passportConfig");
-// const githubAuthRoutes = require("./github-auth/githubAuth");
-
-
-
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const db = require("./Database/mySql");
+const User = require("./Model/user");
+const profile = require("./Model/userProfile");
+const matchesRequest = require("./Model/matchesRequest");
+const userMatchesRoutes = require("./Routes/userMatches");
+const userMatches = require("./Model/matches");
+const userRoute = require("./Routes/userRoutes");
+const session = require("express-session");
+const passport = require("./Controller/githubAuth");
+const githubUserRoutes = require("./Routes/githuUserRoutes");
+const homePageRoutes = require("./Routes/homepage");
+const matchesRequestroutes = require("./Routes/matchesRequestRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
+const aiBotRoutes = require("./Routes/botRoutes");
+const setupSocketServer = require("./Middlewares/sockets");
+const cron = require("node-cron");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const server = http.createServer(app);
-
 const io = setupSocketServer(server);
 
-
+// Sequelize Relationships
 User.hasOne(profile);
 profile.belongsTo(User);
 User.hasMany(matchesRequest);
 matchesRequest.belongsTo(User);
 
-
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+// Sessions and Passport
+app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: false, 
+  saveUninitialized: true 
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/auth", githubUserRoutes);
 
-
-
-
-db.sync()  // or { alter: true }
+// Sync DB
+db.sync()
   .then(() => {
     console.log("Database & tables created!");
   })
   .catch(err => console.error("Error syncing database:", err));
-app.use(bodyParser.json())
-app.use(userRoute)
-app.use(homePageRoutes)
-app.use(matchesRequestroutes)
-app.use(userMatchesRoutes)
-app.use(messageRoutes)
-app.use(aiBotRoutes)
 
+// Routes
+app.use(bodyParser.json());
+app.use(userRoute);
+app.use(homePageRoutes);
+app.use(matchesRequestroutes);
+app.use(userMatchesRoutes);
+app.use(messageRoutes);
+app.use(aiBotRoutes);
 
+// Cron job
 cron.schedule('*/10 * * * *', () => {
-  console.log('running a task every two minutes');
+  console.log('Running a task every 10 minutes');
 });
 
-server.listen(3000)
+// Start server
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
