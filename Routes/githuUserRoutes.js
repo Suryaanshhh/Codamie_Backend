e9968fd2fa198af3bfbadcc42adf06b2dcbd7d7d
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 17:20:00 +0530

    debugging

--- Routes/githuUserRoutes.js
+++ Routes/githuUserRoutes.js
@@ -12,33 +12,51 @@ router.get(
     passport.authenticate("github", { failureRedirect: "/" }),
     async (req, res) => {
         try {
-            const { id, displayName } = req.user
-            console.log(id, displayName)
-            let user = await User.findOne({ where: { githubID: id } })
+            const { id, displayName } = req.user;
+            let user = await User.findOne({ where: { githubID: id } });
+            let token;
 
             if (!user) {
                 let newUser = await User.create({
                     Name: displayName,
                     githubID: id
-                })
-                const token = await jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra")
-                
-                res.redirect(`http://localhost:5173/createProfile?token=${token}`);
+                });
+                token = await jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra");
+                // Send HTML with auto-redirect instead of direct redirect
+                return res.send(`
+                    <html>
+                        <head>
+                            <script>
+                                window.location.href = 'http://localhost:5173/createProfile?token=${token}';
+                            </script>
+                        </head>
+                        <body>
+                            <p>Redirecting to application...</p>
+                        </body>
+                    </html>
+                `);
+            } else {
+                token = await jwt.sign({ userID: user.dataValues.id }, "abra ka dabra");
+                // Send HTML with auto-redirect instead of direct redirect
+                return res.send(`
+                    <html>
+                        <head>
+                            <script>
+                                window.location.href = 'http://localhost:5173/home?token=${token}';
+                            </script>
+                        </head>
+                        <body>
+                            <p>Redirecting to application...</p>
+                        </body>
+                    </html>
+                `);
             }
-
-            else {
-                const token = await jwt.sign({ userID: user.dataValues.id }, "abra ka dabra")
-                res.redirect(`http://localhost:5173/home?token=${token}`);
-            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "something went wrong" });
         }
-        catch (err) {
-            console.log(err)
-            res.status(500).json({ message: "somthing went wrong" })
-        }
-
     }
 );
-
 
 // router.get("/profile", (req, res) => {
 //     if (!req.isAuthenticated()) return res.redirect("/");
