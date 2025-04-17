2b13d2f6c3313516acbeeef57abfbd33cb7b6182
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 16:01:06 +0530

    redirect issue fixed

--- Routes/githuUserRoutes.js
+++ Routes/githuUserRoutes.js
@@ -3,7 +3,7 @@ const passport = require("../Controller/githubAuth");
 const User = require("../Model/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-
+router.options("*", (_, res) => res.sendStatus(200));
 router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
 
 
@@ -11,34 +11,31 @@ router.get(
     "/github/callback",
     passport.authenticate("github", { failureRedirect: "/" }),
     async (req, res) => {
-        try {
-            const { id, displayName } = req.user
-            console.log(id, displayName)
-            let user = await User.findOne({ where: { githubID: id } })
-
-            if (!user) {
-                let newUser = await User.create({
-                    Name: displayName,
-                    githubID: id
-                })
-                const token = await jwt.sign({ userID: newUser.dataValues.id }, "abra ka dabra")
-                
-                res.redirect(`http://localhost:5173/createProfile?token=${token}`);
-            }
-
-            else {
-                const token = await jwt.sign({ userID: user.dataValues.id }, "abra ka dabra")
-                res.redirect(`http://localhost:5173/home?token=${token}`);
-            }
+      try {
+        const { id, displayName } = req.user;
+        let user = await User.findOne({ where: { githubID: id } });
+  
+        if (!user) {
+          user = await User.create({
+            Name: displayName,
+            githubID: id
+          });
         }
-        catch (err) {
-            console.log(err)
-            res.status(500).json({ message: "somthing went wrong" })
-        }
-
+  
+        const token = jwt.sign({ userID: user.dataValues.id }, "abra ka dabra");
+  
+        // Redirect only if from browser
+        const redirectURL = user.isNewRecord
+          ? `http://localhost:5173/createProfile?token=${token}`
+          : `http://localhost:5173/home?token=${token}`;
+  
+        return res.redirect(redirectURL);
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "something went wrong" });
+      }
     }
-);
-
+  );
 
 // router.get("/profile", (req, res) => {
 //     if (!req.isAuthenticated()) return res.redirect("/");
