d7ac0d9cf7fc47ece0e1581e1439d8b08f1645b5
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-04-17 17:24:38 +0530

    handling options

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.options('*', cors());
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
